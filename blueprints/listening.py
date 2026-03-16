fb86bb76688046062eabe8216ecde72b1104201c
2003-40 <1028359607@qq.com>
2026-03-16 14:27:42 +0800

    delete the process view, add calender reminder.

--- blueprints/listening.py
+++ blueprints/listening.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, render_template
-from flask_login import login_required, current_user
-from models import db, ListeningExercise, UserActivityLog
+from flask_login import login_required
+from models import ListeningExercise
 
 listening_bp = Blueprint('listening', __name__, url_prefix='/listening')
 
@@ -9,9 +9,5 @@ listening_bp = Blueprint('listening', __name__, url_prefix='/listening')
 @login_required
 def index():
     exercises = ListeningExercise.query.order_by(ListeningExercise.difficulty).all()
-
-    log = UserActivityLog(user_id=current_user.id, module='listening', action='viewed')
-    db.session.add(log)
-    db.session.commit()
 
     return render_template('listening/index.html', exercises=exercises)
