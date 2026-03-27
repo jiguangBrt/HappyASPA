3796a658e26fc1bf7678b99bf99b112ae1eda531
2003-40 <1028359607@qq.com>
2026-03-27 08:37:26 +0800

    merge multiple haeds

---
+++ migrations/versions/65b01b6f68cd_merge_heads.py
@@ -0,0 +1,24 @@
+"""merge heads
+
+Revision ID: 65b01b6f68cd
+Revises: 5e91972af4f7, c43e16d45f52
+Create Date: 2026-03-27 08:31:33.591088
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '65b01b6f68cd'
+down_revision = ('5e91972af4f7', 'c43e16d45f52')
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
